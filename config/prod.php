8a6b4a66d1994ecea1de828adf557d15b78e1960
Romain Pouclet <palleas@gmail.com>
2014-11-07 08:41:20 -0500

    Add API KEY in prod config

--- config/prod.php
+++ config/prod.php
@@ -4,3 +4,6 @@
 
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+$app['marvel.api_key'] = 'ad4226afe4d476d598b8ebfb4086d5fc';
+$app['marvel.api_secret'] = '35a89025a3f9182626089bcdbd51e82b4305eeac';
