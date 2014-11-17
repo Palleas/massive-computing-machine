0b8fa58fc513974bb9287204cae739cdadb8edce
Romain Pouclet <palleas@gmail.com>
2014-11-17 16:35:06 -0500

    Debug in production #YOLO

--- config/prod.php
+++ config/prod.php
@@ -5,4 +5,6 @@
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
 
+$app['debug'] = true;
+
 $app['comicvine.api_key'] = '291bd197c96f8f42ba79bd157198a4bbbcb204f1';
