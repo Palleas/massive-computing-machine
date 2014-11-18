b5104b2beb68df9b5d4f9ff33cb7f8de6991c192
Romain Pouclet <palleas@gmail.com>
2014-11-18 08:52:37 -0500

    Setup post install scripts

--- src/Powermash/Powermash.php
+++ src/Powermash/Powermash.php
@@ -13,6 +13,10 @@ class Powermash
 	public static function setupDatabase(Event $event)
 	{
 		$path = __DIR__.'/../../data/app.db.dist';
+		if (!file_exists($path)) {
+			throw new \RuntimeException(sprintf('Database file does not exist at path %s', $path));
+		}
+		
 		copy($path, __DIR__.'/../../data/app.db');
 	}
 }
